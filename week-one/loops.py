5e01bc7ab5f8e864c5f04f7cb59d7b8b44f556d6
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-05 20:50:54 +0100

    loops

--- week-one/loops.py
+++ week-one/loops.py
@@ -11,3 +11,7 @@ for val in numbers:
     sum = sum+val
 
 print("The sum is", sum)
+print(range(10)) #range(0, 10)
+print(list(range(10))) #[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(list(range(2, 8))) #[2, 3, 4, 5, 6, 7]
+print(list(range(2, 20, 3))) #[2, 5, 8, 11, 14, 17]
