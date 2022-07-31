9d4b70e9ed9a554555961b15001b2ab0c5da7098
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-01 00:20:01 +0100

    creating our first Class

--- oop/task1.py
+++ oop/task1.py
@@ -3,9 +3,26 @@ class Point:
         self.x = x
         self.y = y
 
+    def reset(self):
+        self.x = 0
+        self.y = 0
+
+    def move(self, newx, newy):
+        self.x = newx
+        self.y = newy
+
+    def movX(self, newx):
+        self.x = newx
+
+    def movY(self, newy):
+        self.y = newy
+
 ## Main Program
 p1 = Point()
 p2 = Point(3, -4)
 
+p2.move(5,5)
+
 print(p1.x, p1.y)
 print(p2.x, p2.y)
+
