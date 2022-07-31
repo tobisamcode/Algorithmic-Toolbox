163bb9b8c526d7890a17750ed94fedb5bbd039a7
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-01 00:27:36 +0100

    creating our first Class

--- oop/task1.py
+++ oop/task1.py
@@ -1,7 +1,13 @@
+from math import sqrt
+
 class Point:
     def __init__(self, x = 0, y = 0):
         self.x = x
         self.y = y
+
+    @property
+    def mag(self):
+        return sqrt(self.x**2 + self.y**2)
 
     def reset(self):
         self.x = 0
@@ -17,12 +23,14 @@ class Point:
     def movY(self, newy):
         self.y = newy
 
+    def showPoint(self):
+        return f'The points are ({self.x}, {self.y})'
+
 ## Main Program
 p1 = Point()
 p2 = Point(3, -4)
 
 p2.move(5,5)
 
-print(p1.x, p1.y)
-print(p2.x, p2.y)
-
+print(p2.mag)
+print(p2.showPoint())
