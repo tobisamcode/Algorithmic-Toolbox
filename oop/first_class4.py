2aa2955c297b54243b78f23d76906f4f2e40edd8
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-31 02:16:07 +0100

    creating our first Class

--- oop/first_class4.py
+++ oop/first_class4.py
@@ -1,8 +1,19 @@
 class Student:
+    department = 'Mechatronics'
+    offSubjects = ['Mech', 'LA', 'ES', 'CP-II', 'MOM', 'Proj']
     def __init__(self, fName, lName, reg):
         self.fName = fName
         self.lName = lName
         self.reg = reg
+        self.email = f'{self.reg.lower()}@uet.edu.ng'
+        self.courses = ['Proj']
+    
+    def registerSubject (self, *sub): 
+        for s in sub:
+            if s not in Student.offSubjects:
+                raise ValueError(f'{s} is not offered')
+            if s in Student.offSubjects and s not in self.courses:
+                self.courses.append(s)
 
 
 ## main program ##
@@ -10,5 +21,7 @@ std1 = Student('Tobi', 'Adesokan', 'MCT-UET-001') # Object Instantiated and Obje
 std2 = Student('Banji', 'Egunjobi', 'MCT-UET-002')
 
 
-print(std1.fName)
-print(std2.reg)
+std1.registerSubject('CP-II', 'MOM')
+print(std1.courses)
+
+
