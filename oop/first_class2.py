bf85a559b1a5a95ea5f8c7175874c22596b13538
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-31 01:29:15 +0100

    creating our first Class

---
+++ oop/first_class2.py
@@ -0,0 +1,17 @@
+class Student:
+    def initialize(obj, f, l, r):
+        obj.fName = f
+        obj.lName = l
+        obj.reg = r
+
+
+## main program ##
+std1 = Student() # Object Instantiated
+std2 = Student()
+
+# Object initailization
+Student.initialize(std1, 'Tobi', 'Adesokan', 'MCT-UET-001')
+Student.initialize(std2, 'Banji', 'Egunjobi', 'MCT-UET-002')
+
+print(std1.fName)
+print(std2.reg)
