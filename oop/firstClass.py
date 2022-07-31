bb29d00f5685e8007d2609b272cd065e104ec835
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-31 01:17:38 +0100

    creating our first Class

---
+++ oop/firstClass.py
@@ -0,0 +1,19 @@
+class Student:
+    pass
+
+
+## main program ##
+std1 = Student() # Object Instantiated
+std2 = Student()
+
+# Object initailization
+std1.fName = 'Tobi'
+std1.lName = "Adesokan"
+std1.reg = 'MCT-UET-001'
+
+std2.fName = 'Banji'
+std2.lName ='Egunjobi'
+std2.reg = 'MCT-UET-002'
+
+print(std1.fName)
+print(std2.reg)
